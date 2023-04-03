f3b5557bdd2233167321b46cc318a3f6f14d8889
kimalss22 <kimalss22@gmail.com>
2023-04-03 12:45:45 +0900

    ãDio Repository 사용

--- lib/post_repository.dart
+++ lib/post_repository.dart
@@ -4,8 +4,13 @@ import 'package:dio/dio.dart';
 class PostRepository {
   final dio = Dio();
   
-  Future<void> findByIdV1(int id) async {
+  Future<void> findById(int id) async {
     Response response = await dio.get("https://jsonplaceholder.typicode.com/posts/$id");
+    print(response.data);
+  }
+
+  Future<void> findAll() async {
+    Response response = await dio.get("https://jsonplaceholder.typicode.com/posts");
     print(response.data);
   }
 }
